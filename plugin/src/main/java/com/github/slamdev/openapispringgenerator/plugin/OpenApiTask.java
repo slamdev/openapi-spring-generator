9fe18e6200dbf8b949bdceaeca6a153901d9403d
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2020-11-08 19:32:13 +0100

    refactor plugin

--- plugin/src/main/java/com/github/slamdev/openapispringgenerator/plugin/OpenApiTask.java
+++ plugin/src/main/java/com/github/slamdev/openapispringgenerator/plugin/OpenApiTask.java
@@ -12,19 +12,47 @@ import org.gradle.internal.progress.PercentageProgressFormatter;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Properties;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 public class OpenApiTask extends DefaultTask {
 
-    private final List<List<File>> clients = new ArrayList<>();
+    private static class Spec {
+        private final Path file;
+        private final Generator.Type type;
 
-    private final List<List<File>> servers = new ArrayList<>();
+        private Spec(Path file, Generator.Type type) {
+            this.file = file;
+            this.type = type;
+        }
+
+        public Path getFile() {
+            return file;
+        }
+
+        public Generator.Type getType() {
+            return type;
+        }
+    }
+
+    private final List<Spec> specs = new ArrayList<>();
+
+    private final ProgressLoggerFactory progressLoggerFactory;
+
+    private File destinationDir;
+
+    @Inject
+    public OpenApiTask(ProgressLoggerFactory progressLoggerFactory) {
+        this.progressLoggerFactory = progressLoggerFactory;
+    }
 
     @OutputDirectory
     public File getDestinationDir() {
@@ -35,129 +63,115 @@ public class OpenApiTask extends DefaultTask {
         this.destinationDir = destinationDir;
     }
 
-    private File destinationDir;
-
-    private final ProgressLoggerFactory progressLoggerFactory;
-
-    @Inject
-    public OpenApiTask(ProgressLoggerFactory progressLoggerFactory) {
-        this.progressLoggerFactory = progressLoggerFactory;
-    }
-
     public void client(File file) {
-        clients.add(Collections.singletonList(file));
+        specs.add(new Spec(file.toPath(), Generator.Type.CLIENT));
     }
 
     public void client(FileCollection fileCollection) {
-        clients.add(new ArrayList<>(fileCollection.getFiles()));
+        fileCollection.getFiles().stream()
+                .map(File::toPath)
+                .map(f -> new Spec(f, Generator.Type.CLIENT))
+                .forEach(specs::add);
     }
 
     public void server(File file) {
-        servers.add(Collections.singletonList(file));
+        specs.add(new Spec(file.toPath(), Generator.Type.SERVER));
     }
 
     public void server(FileCollection fileCollection) {
-        servers.add(new ArrayList<>(fileCollection.getFiles()));
-    }
-
-    @Internal
-    protected List<File> getSpecs() {
-        List<File> all = new ArrayList<>();
-        clients.stream().flatMap(List::stream).forEach(all::add);
-        servers.stream().flatMap(List::stream).forEach(all::add);
-        return all;
+        fileCollection.getFiles().stream()
+                .map(File::toPath)
+                .map(f -> new Spec(f, Generator.Type.SERVER))
+                .forEach(specs::add);
     }
 
     @InputFiles
+    @SkipWhenEmpty
     @PathSensitive(PathSensitivity.RELATIVE)
-    protected List<File> getClientSpecs() {
-        return clients.stream().flatMap(List::stream).collect(Collectors.toList());
-    }
-
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE)
-    protected List<File> getServerSpecs() {
-        return servers.stream().flatMap(List::stream).collect(Collectors.toList());
-    }
-
-    private void move(FileTree tree, String dir) {
-        File tempDir = new File(destinationDir, "temp");
-        Path newDir = destinationDir.toPath().resolve("main").resolve(dir);
-        for (File file : tree.getFiles()) {
-            Path fileName = tempDir.toPath().relativize(file.toPath());
-            Path newFile = newDir.resolve(fileName);
-            try {
-                if (!Files.exists(newFile.getParent())) {
-                    Files.createDirectories(newFile.getParent());
-                }
-                Files.move(file.toPath(), newFile);
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
-            }
-        }
+    public List<File> getSpecs() {
+        return specs.stream()
+                .map(Spec::getFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
     }
 
     @TaskAction
-    public void generate() {
+    public void generate() throws IOException {
+        if (Files.exists(destinationDir.toPath())) {
+            Files.walk(destinationDir.toPath())
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        }
+
+        Path tempDir = getTemporaryDir().toPath();
+
         ProgressLogger progressLogger = progressLoggerFactory.newOperation(OpenApiTask.class);
-        progressLogger.start("Swagger code generation", null);
-
+        progressLogger.start("OpenApi code generation", null);
         try {
-            PercentageProgressFormatter progressFormatter = new PercentageProgressFormatter("Generating", getSpecs().size() + 2);
-            progressLogger.progress(progressFormatter.incrementAndGetProgress());
-            delete(destinationDir);
-            progressLogger.progress(progressFormatter.incrementAndGetProgress());
-
-            File tempDir = new File(destinationDir, "temp");
-
-            tempDir.mkdirs();
-
-            servers.forEach(files -> {
-                if (files.isEmpty()) {
-                    return;
-                }
+            PercentageProgressFormatter progressFormatter = new PercentageProgressFormatter("Generating", specs.size());
+            for (Spec spec : specs) {
                 progressLogger.progress(progressFormatter.getProgress());
-                files.stream()
-                        .map(File::toPath)
-                        .forEach(specFile -> new Generator().generate(specFile, Generator.Type.SERVER, tempDir.toPath()));
+                new Generator().generate(spec.getFile(), spec.getType(), tempDir);
+
+                FileTree javaTree = (FileTree) getProject().fileTree(tempDir).include("**/*.java");
+                move(javaTree, "java", (src, dest) -> {
+                    throw new IllegalStateException("" + dest + " already exists");
+                });
+
+                FileTree resourcesTree = (FileTree) getProject().fileTree(tempDir).exclude("**/*.java");
+                move(resourcesTree, "resources", (src, dest) -> {
+                    if (!src.getFileName().toString().equals("spring.factories")) {
+                        throw new IllegalStateException("" + dest + " already exists");
+                    }
+                    mergeSpringFactories(src, dest);
+                });
+
                 progressFormatter.increment();
-            });
-            clients.forEach(files -> {
-                if (files.isEmpty()) {
-                    return;
-                }
-                progressLogger.progress(progressFormatter.getProgress());
-                files.stream()
-                        .map(File::toPath)
-                        .forEach(specFile -> new Generator().generate(specFile, Generator.Type.CLIENT, tempDir.toPath()));
-                progressFormatter.increment();
-            });
-            FileTree javaTree = (FileTree) getProject().fileTree(tempDir).include("**/*.java");
-            move(javaTree, "java");
-            FileTree resourcesTree = (FileTree) getProject().fileTree(tempDir).exclude("**/*.java");
-            move(resourcesTree, "resources");
-            delete(tempDir);
+            }
         } finally {
             progressLogger.completed();
         }
     }
 
-    private void delete(File f) {
-        Path file = f.toPath();
-        if (!Files.exists(file)) {
-            return;
-        }
+    private void mergeSpringFactories(Path src, Path dest) {
         try {
-            if (Files.isDirectory(file)) {
-                Files.walk(file)
-                        .sorted(Comparator.reverseOrder())
-                        .map(Path::toFile)
-                        .forEach(File::delete);
-            } else {
-                Files.delete(file);
+            Properties srcProps = new Properties();
+            srcProps.load(Files.newBufferedReader(src));
+
+            Properties destProps = new Properties();
+            destProps.load(Files.newBufferedReader(dest));
+
+            for (String key : srcProps.stringPropertyNames()) {
+                if (destProps.containsKey(key)) {
+                    String merged = destProps.getProperty(key) + "," + srcProps.getProperty(key);
+                    destProps.put(key, merged);
+                } else {
+                    destProps.put(key, srcProps.getProperty(key));
+                }
             }
+
+            destProps.store(Files.newBufferedWriter(dest, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING), null);
+
+            Files.delete(src);
         } catch (IOException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    private void move(FileTree tree, String dir, BiConsumer<Path, Path> merge) throws IOException {
+        Path newDir = destinationDir.toPath().resolve("main").resolve(dir);
+        for (File file : tree.getFiles()) {
+            Path fileName = getTemporaryDir().toPath().relativize(file.toPath());
+            Path newFile = newDir.resolve(fileName);
+            if (!Files.exists(newFile.getParent())) {
+                Files.createDirectories(newFile.getParent());
+            }
+            if (Files.exists(newFile)) {
+                merge.accept(file.toPath(), newFile);
+            } else {
+                Files.move(file.toPath(), newFile);
+            }
         }
     }
 }
---
+++ plugin/src/test/java/com/github/slamdev/openapispringgenerator/plugin/OpenApiSpringGeneratorPluginTest.java
@@ -0,0 +1,34 @@
+package com.github.slamdev.openapispringgenerator.plugin;
+
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class OpenApiSpringGeneratorPluginTest {
+
+    private OpenApiTask task;
+
+    @Before
+    public void setUp() {
+        Project project = ProjectBuilder.builder().build();
+        project.getPluginManager().apply(JavaPlugin.class);
+        project.getPluginManager().apply(OpenApiSpringGeneratorPlugin.class);
+        task = (OpenApiTask) project.getTasks().getByName("openapi");
+    }
+
+    @Test
+    public void should_run_task() throws IOException {
+        task.client(file("spec.yaml"));
+        task.generate();
+    }
+
+    private File file(String name) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource(name).getFile());
+    }
+}
