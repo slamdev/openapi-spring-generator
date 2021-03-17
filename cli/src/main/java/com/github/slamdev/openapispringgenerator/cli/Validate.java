41e31cd2b3248ce239e3828945b5d5d1c4cdfc9a
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2021-03-17 20:45:53 +0100

    allow multiple spec files for validate command

--- cli/src/main/java/com/github/slamdev/openapispringgenerator/cli/Validate.java
+++ cli/src/main/java/com/github/slamdev/openapispringgenerator/cli/Validate.java
@@ -10,15 +10,29 @@ import java.util.List;
 @CommandLine.Command(name = "validate", mixinStandardHelpOptions = true, description = "validate spec file")
 public class Validate implements Runnable {
 
-    @CommandLine.Option(names = {"-f", "--file"}, required = true, description = "path to a spec file")
-    private Path specFile;
+    @CommandLine.Parameters(arity = "1..*", description = "path to a spec file")
+    private List<Path> specFiles;
+
+    @CommandLine.Option(names = {"--fail-on-errors"}, description = "fail cli if errors found")
+    private boolean failOnErrors;
 
     @Override
-    @SuppressWarnings("PMD.SystemPrintln")
+    @SuppressWarnings({"PMD.SystemPrintln", "PMD.DoNotTerminateVM"})
     public void run() {
-        List<Invalid> results = new Validator().validate(specFile);
-        for (Invalid result : results) {
-            System.err.printf("%s: %s%n", result.getRule().getSeverity(), result.getMessage());
+        boolean failed = false;
+        for (Path specFile : specFiles) {
+            List<Invalid> results = new Validator().validate(specFile);
+            if (results.isEmpty()) {
+                continue;
+            }
+            failed = true;
+            System.err.printf("errors found in %s%n", specFile.toString());
+            for (Invalid result : results) {
+                System.err.printf("%s: %s%n", result.getRule().getSeverity(), result.getMessage());
+            }
+        }
+        if (failOnErrors && failed) {
+            System.exit(1);
         }
     }
 }
