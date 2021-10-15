77ee3c03225e208dc2c07a44d85dc7c32f27bb66
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2021-10-15 16:22:10 +0200

    update dependencies

--- plugin/src/main/java/com/github/slamdev/openapispringgenerator/plugin/OpenApiSpringGeneratorPlugin.java
+++ plugin/src/main/java/com/github/slamdev/openapispringgenerator/plugin/OpenApiSpringGeneratorPlugin.java
@@ -3,9 +3,10 @@ package com.github.slamdev.openapispringgenerator.plugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.language.jvm.tasks.ProcessResources;
 
 import java.io.File;
 
@@ -18,13 +19,14 @@ public class OpenApiSpringGeneratorPlugin implements Plugin<Project> {
         project.getPluginManager().apply(JavaPlugin.class);
         OpenApiTask task = project.getTasks().create("openapi", OpenApiTask.class, t -> t.setDestinationDir(calculateDestination(project)));
         project.getTasks().withType(JavaCompile.class, t -> t.dependsOn(task));
-        JavaPluginConvention convention = project.getConvention().getPlugin(JavaPluginConvention.class);
-        SourceSet sourceSet = convention.getSourceSets().getByName("main");
+        project.getTasks().withType(ProcessResources.class, t -> t.dependsOn(task));
+        JavaPluginExtension extension = project.getExtensions().getByType(JavaPluginExtension.class);
+        SourceSet sourceSet = extension.getSourceSets().getByName("main");
         sourceSet.java(dir -> dir.srcDir(task.getDestinationDir().toString() + "/main/java"));
         sourceSet.resources(dir -> dir.srcDir(task.getDestinationDir().toString() + "/main/resources"));
     }
 
     private File calculateDestination(Project project) {
-        return project.file(project.getBuildDir().toString() + "/" + PATH);
+        return project.file(project.getBuildDir() + "/" + PATH);
     }
 }
